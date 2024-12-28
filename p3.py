e1e2b80229377d519ca03fad985dd5ecf07970a6
pirshariq <pirsulieman@gmail.com>
2024-12-28 13:21:30 +0530

    Update and rename simple.py to p3.py

---
+++ p3.py
@@ -0,0 +1,6 @@
+num1 = 15.5
+num2 = 24.3
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
--- simple.py
+++
@@ -1,6 +0,0 @@
-num1 = 15.5
-num2 = 24.3
-
-sum = num1 + num2
-
-print(f"The sum of {num1} and {num2} is: {sum}")
